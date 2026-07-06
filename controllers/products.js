b54d5afd9c0e776b6c17aeda26730b9543997bab
Jacky <jackylai1030@gmail.com>
2026-07-06 13:35:49 -0400

    completed createProduct function

--- controllers/products.js
+++ controllers/products.js
@@ -1,32 +1,48 @@
-const createProduct = async (req, res) => {
-    res.send('createProduct placeholder');
+import mongoose from "mongoose";
+import Product from "../models/product.js";
+
+export const createProduct = async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields" });
+  }
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in Create product:", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 };
 
 // member 2 task placeholder
-  const getProducts = async (req, res) => {
-    res.send('getProducts placeholder');
-  };
+const getProducts = async (req, res) => {
+  res.send("getProducts placeholder");
+};
 
- // member3 task placeholder
- 
-  const getProductbyId = async (req, res) => {
-    res.send('getProductbyId placeholder');
-  };
+// member3 task placeholder
 
-  // member 4 task placeholder
-    const updateProduct = async (req, res) => {
-    res.send('updateProduct placeholder');
+const getProductbyId = async (req, res) => {
+  res.send("getProductbyId placeholder");
+};
 
-    };
-  // member 5 placeholder
-    const deleteProduct = async (req, res) => {
-    res.send('deleteProduct placeholder'); 
-    }; 
+// member 4 task placeholder
+const updateProduct = async (req, res) => {
+  res.send("updateProduct placeholder");
+};
+// member 5 placeholder
+const deleteProduct = async (req, res) => {
+  res.send("deleteProduct placeholder");
+};
 
-    module.exports = {
-        createProduct,
-        getProducts,
-        getProductbyId,
-        updateProduct,
-        deleteProduct
-    };
+module.exports = {
+  createProduct,
+  getProducts,
+  getProductbyId,
+  updateProduct,
+  deleteProduct,
+};
