1ab39c7f81aef4be5b12d7a8ab2292e0e7426bbb
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-07-06 13:12:31 -0400

    add new file

---
+++ controllers/products.js
@@ -0,0 +1,32 @@
+const createProduct = async (req, res) => {
+    res.send('createProduct placeholder');
+};
+
+// member 2 task placeholder
+  const getProducts = async (req, res) => {
+    res.send('getProducts placeholder');
+  };
+
+ // member3 task placeholder
+ 
+  const getProductbyId = async (req, res) => {
+    res.send('getProductbyId placeholder');
+  };
+
+  // member 4 task placeholder
+    const updateProduct = async (req, res) => {
+    res.send('updateProduct placeholder');
+
+    };
+  // member 5 placeholder
+    const deleteProduct = async (req, res) => {
+    res.send('deleteProduct placeholder'); 
+    }; 
+
+    module.exports = {
+        createProduct,
+        getProducts,
+        getProductbyId,
+        updateProduct,
+        deleteProduct
+    };
