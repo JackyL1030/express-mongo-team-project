15d3d532398630483ea75abcb0d828e85db8601e
Jacky <jackylai1030@gmail.com>
2026-07-06 13:26:15 -0400

    index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config(); 
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.json());
+
+app.get("/", (req,res) => {
+    res.send("API is running")
+})
+
+app.listen(PORT, () =>{
+    console.log("Server started at http://localhost:" + PORT);
+})
